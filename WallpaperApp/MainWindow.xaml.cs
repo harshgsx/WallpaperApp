8631de084c7964aee0596e8c6a9b98e65bbe53bf
Harsh Shah <harshshah8519@hotmail.com>
2019-03-30 00:42:10 +0530

    Initial commit with wallpaper change on button click.
    @harshshah903

---
+++ WallpaperApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WallpaperApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSetWallpaper_Click(Object sender, RoutedEventArgs e)
+        {
+            
+            Wallpaper.Set(@"C:\Users\HarshTestbench\Pictures\wp2208083.jpg", Wallpaper.Style.Fill);
+        }
+    }
+}
---
+++ WallpaperApp/Wallpaper.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace WallpaperApp
+{
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto )]
+        static extern int SystemParametersInfo(int uAction, int uParamm , String lpvparamram, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched,
+            Fill,
+            Fit,
+            Span
+        }
+
+        public static void Set(String FilePath, Style style)
+        {
+            System.IO.Stream s = File.OpenRead(FilePath);           
+            System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+            
+            string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+            s.Close();
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", "2");
+                key.SetValue(@"TileWallpaper", "0");
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", "3");
+                key.SetValue(@"TileWallpaper", "0");
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", "1");
+                key.SetValue(@"TileWallpaper", "1");
+            }
+            if(style == Style.Fit)
+            {
+                key.SetValue(@"WallpaperStyle", "6");
+                key.SetValue(@"TileWallpaper", "0");
+            }
+            if (style == Style.Fill)
+            {
+                key.SetValue(@"WallpaperStyle", "10");
+                key.SetValue(@"TileWallpaper", "0");
+            }
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+    }
+
+}
+
